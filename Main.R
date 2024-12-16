b9df957e86ad96e9e9ecc55ee6e3fa5eca96811f
Qifan Zhang <qifansz1008@gmail.com>
2024-12-16 14:02:02 -0500

    Add conda environment setup and enhance counterfactual dataset generation

--- Main.R
+++ Main.R
@@ -1,6 +1,7 @@
 system("mkdir -p processed_data")
 system("mkdir -p raw_data")
 system("mkdir -p models")
+system("conda env create -f environment.yml")
 
 source("ObtainData.R")
 source("PrepData.R")
